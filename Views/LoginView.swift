27e06e85b81f598a1fe54dd9fca2cc3a00c031c0
Stanislav Schults <stschults@yandex.ru>
2023-05-22 20:27:47 +0300

    Added LoginView

--- Views/LoginView.swift
+++ Views/LoginView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    Button {
+                        // Attempt log in
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            Text("log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                // Create Acount
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create An Account",
+                                   destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
